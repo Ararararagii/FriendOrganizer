f0baec780119beb94ca789bc2cf0ce91dd82793e
kamil <kamil@DESKTOP-7IGQUM7>
2024-01-09 21:30:06 +0100

    Add project files.

---
+++ FriendOrganizerUI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizerUI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            //TODO: Load real data from real database
+            yield return new Friend { FirstName = "Kamil", LastName = "G" };
+            yield return new Friend { FirstName = "Michał", LastName = "Różycki" };
+            yield return new Friend { FirstName = "Tomek", LastName = "Chodak" };
+            yield return new Friend { FirstName = "Marek", LastName = "Bernatowicz" };
+        }
+    }
+}
